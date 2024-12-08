3397bb33d75e6fbc039ec6bbf1ba3b08eeb499f5
Momin-cses <mominwebdeveloper@gmail.com>
2024-12-08 10:43:08 +0530

    added internal documentation in backend

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -5,19 +5,24 @@ const { loginDoctor } = require("../controllers/doctorAuth");
 const router = express.Router();
 
 // Registration route
+// http://localhost5000/api/auth/register 
 router.post("/register", registerStudent);
 
 // Email verification route
+// http://localhost5000/api/auth/verify 
 router.get("/verify", verifyEmail);
 
 //Login route
+// http://localhost5000/api/auth/login 
 router.post("/login", loginStudent);
 
 //student data fetching
+// http://localhost5000/api/auth/fetchStudent 
 router.get("/fetchStudent", authenticateToken,fetchStudent);
 
 
 //login doctor
+// http://localhost5000/api/auth/doctor/login 
 router.post("/doctor/login", loginDoctor);
 
 module.exports = router;
